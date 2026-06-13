74046091c99d3056e018a87fa7367dd03af8c05e
Deepak98260 <Deepaksingh98260@gmail.com>
2026-06-13 17:24:29 +0530

    header-section

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
+
+const PUBLISHABLE_KEY = "YOUR_CLERK_PUBLISHABLE_KEY"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-   <App />
-   </BrowserRouter>
-   
+  
+    <ClerkProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   
 )
